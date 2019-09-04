6cad03348068fe4bff28615264a57d884ca4e78f
Piyush Sawarkar <piyush.s.sawarkar@gmail.com>
2019-09-04 11:45:07 +0530

    Update reverse_a_string.c

--- reverse_a_string.c
+++ reverse_a_string.c
@@ -5,3 +5,14 @@ int main(){
     strcpy(rev,"");
     for(int i=0;rev[strlen(orig)-i-1]=orig[i];i++,orig[i]=='\0'?rev[i]='\0',printf("The Reverse is %s",rev):1);
 }
+/*
+In this Code:
+just replace content of "orig" with whatever string you want and reverse of it would be printed on the screen when you execute the above piece of code
+*/
+/*
+Compilation and execution Procedure:
+Fire up Terminal/or may be bash(wsl) on windows
+type...
+$ gcc reverse_a_string.c -o rev
+$ ./rev
+*/
